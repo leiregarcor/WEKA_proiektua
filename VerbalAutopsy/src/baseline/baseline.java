e46e837cfe99ca7e479aad4bdacfb6659db16fc3
AitorSanJose1 <aitorsanjoseheras@gmail.com>
2022-04-01 11:58:51 +0200

    baseline done

--- VerbalAutopsy/src/baseline/baseline.java
+++ VerbalAutopsy/src/baseline/baseline.java
@@ -1,15 +1,42 @@
 package baseline;
 
+import java.io.FileWriter;
+
+import com.googlecode.jfilechooserbookmarks.core.Utils;
+
+import weka.classifiers.Evaluation;
+import weka.classifiers.bayes.NaiveBayes;
 import weka.core.Instances;
 import weka.core.converters.ConverterUtils.DataSource;
 
 public class baseline {
+	
+	static long startTime = System.nanoTime();
 
 	public static void main(String[] args) throws Exception {
-		DataSource ds = new DataSource(args[0]);
-		Instances data = ds.getDataSet();
+		DataSource dsTrain = new DataSource(args[0]);
+		Instances train = dsTrain.getDataSet();
+		train.setClassIndex(train.numAttributes()-1);
+		DataSource dsTest = new DataSource(args[1]);
+		Instances test = dsTest.getDataSet();
+		test.setClassIndex(test.numAttributes()-1);
+		
+		NaiveBayes nb = new NaiveBayes();
+		nb.buildClassifier(train);
 		
 		
+		Evaluation eval = new Evaluation(train);
+		eval.evaluateModel(nb, test);
+		
+		long endTime   = System.nanoTime();
+        long totalTime = endTime - startTime;
+		int minoritarioa = weka.core.Utils.minIndex(train.attributeStats(train.classIndex()).nominalCounts);
+		
+		FileWriter fw = new FileWriter(args[2]);
+		fw.write("Naive bayes-en exekuzio denbora: " + totalTime);
+		fw.write("\n");
+		fw.write("Klase minoritarioaren f-measure = " + eval.fMeasure(minoritarioa));
+		fw.close();
 
 	}
 
